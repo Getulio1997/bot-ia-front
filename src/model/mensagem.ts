6827928315eb5d395e4db571ff43d86775f2f5b4
Getulio De Souza Monte <506950@pjro.local>
2024-09-25 10:55:33 -0400

    refactor: Melhora a model de mensagem

--- src/model/mensagem.ts
+++ src/model/mensagem.ts
@@ -1,9 +1,11 @@
 export class Mensagem {
-  mensagem: string;
-  resposta?: string;
+  public mensagem: string;
+  public resposta: string | undefined;
+  public dataHora: Date;
 
-  constructor(mensagem: string, resposta?: string) {
+  constructor(mensagem: string) {
     this.mensagem = mensagem;
-    this.resposta = resposta;
+    this.resposta = undefined;
+    this.dataHora = new Date();
   }
 }
